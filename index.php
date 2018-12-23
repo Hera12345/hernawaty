440065b66714bfd5fe25cc46662bd9bef3571bed
Hera12345 <46089675+Hera12345@users.noreply.github.com>
2018-12-24 00:11:57 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
 echo "
 <html> 
-<body background='download.jpg'>
+<body background='download.jpg' style='background-size:cover;
+			background-attachment: fixed;'>
 <center>Assalamu'alaikum Warahmatullahi Wabarakatuh
  <table border=1 style='padding:2;margin-top:20px;'>
   <tr>
